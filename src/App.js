d6f761136caf519bb0114f201348b23efdf6fdd5
carmenkuang87@gmail.com <carmenkuang87@gmail.com>
2022-05-13 19:52:45 -0400

    create light&dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import {light} from "./styles/Themes"
 
 function App() {
   return (
     <>
+    <GlobalStyles />
+      <ThemeProvider theme={light}>
       App file
+      </ThemeProvider>
     </>
   );
 }
