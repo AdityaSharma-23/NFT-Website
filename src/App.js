84c3ed5a2cdc77b0528e05ea471b720cd6f1b37d
carmenkuang87@gmail.com <carmenkuang87@gmail.com>
2022-05-13 22:00:02 -0400

    create components template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import { ThemeProvider } from "styled-components";
+
 import GlobalStyles from "./styles/GlobalStyles";
 import {light} from "./styles/Themes"
+
+import Navigation from "./components/Navigation";
+import Home from './components/sections/Home';
+import About from './components/sections/About';
+import Roadmap from './components/sections/Roadmap';
+import Showcase from './components/sections/Showcase';
+import Team from './components/sections/Team';
+import Faq from './components/sections/Faq';
+import Footer from "./components/Footer";
+
+
 
 function App() {
   return (
     <>
     <GlobalStyles />
       <ThemeProvider theme={light}>
-      App file
+      <Navigation />
+      <Home />
+      <About />
+      <Roadmap />
+      <Showcase />
+      <Team />
+      <Faq />
+      <Footer />
       </ThemeProvider>
     </>
   );
